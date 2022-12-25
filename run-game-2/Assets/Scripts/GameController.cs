125f258b178501fc7d1a43f10f91201b6136aabb
sakkke <w32w64@gmail.com>
2022-12-25 16:39:43 +0900

    feat: add jump feature

--- run-game-2/Assets/Scripts/GameController.cs
+++ run-game-2/Assets/Scripts/GameController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static float playerJumpPower = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ run-game-2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool _jumped;
+    private Rigidbody2D _rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_jumped && Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * GameController.playerJumpPower, ForceMode2D.Impulse);
+            _jumped = true;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (collision2D.gameObject.tag == "Plane")
+        {
+            _jumped = false;
+        }
+    }
+}
