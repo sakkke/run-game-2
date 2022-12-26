fc283eb9268f81bdc62f33653aa9dc6182c324a7
sakkke <w32w64@gmail.com>
2022-12-26 12:34:27 +0900

    feat: add camera scrolling

--- run-game-2/Assets/Scripts/CameraController.cs
+++ run-game-2/Assets/Scripts/CameraController.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class CameraController : MonoBehaviour
 {
-    public static float cameraSpeed = 1;
-    public static float playerJumpPower = 10;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +13,6 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        transform.position += Vector3.right * Time.deltaTime * GameController.cameraSpeed;
     }
 }
