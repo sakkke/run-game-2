e12e6eaa475cc7ee392c89f4510a250fcb7a0231
sakkke <w32w64@gmail.com>
2023-01-22 14:37:44 +0900

    feat: add BirdController

---
+++ run-game-2/Assets/Scripts/BirdController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    GameController _gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        if (!_gameController.IsMultiplayer && collision2D.gameObject.tag == "Player")
+        {
+            _gameController.GameOver();
+        }
+    }
+}
